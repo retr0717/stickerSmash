0478bf1f89d9d0765afcb06e2e33485bb305beb6
retr0717 <alwinaji717@gmail.com>
2024-03-03 14:31:30 +0530

    photo added

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+//image import
+const PlaceHolderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceHolderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
